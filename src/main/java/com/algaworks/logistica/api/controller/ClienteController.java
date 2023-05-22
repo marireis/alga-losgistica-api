082c027d9cf03f7d44bf3c7d3628346eb253c2b3
Marina Reis <marinareis.mr@gmail.com>
2023-05-22 09:19:57 -0300

    endpoint delete

--- src/main/java/com/algaworks/logistica/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/logistica/api/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.algaworks.logistica.api.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,6 +66,16 @@ public class ClienteController {
 		
 		return ResponseEntity.ok(cliente);
 		
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId){
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		clienteRepository.deleteById(clienteId);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
