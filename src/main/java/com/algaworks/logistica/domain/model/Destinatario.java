84ce39404850f83779c0345a493c14ce71137664
Marina Reis <marinareis.mr@gmail.com>
2023-05-22 12:10:39 -0300

    nova migration para tabela entrega

---
+++ src/main/java/com/algaworks/logistica/domain/model/Destinatario.java
@@ -0,0 +1,28 @@
+package com.algaworks.logistica.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Embeddable //capacidade de ser embutida
+public class Destinatario {
+
+	@Column(name = "destinario_nome")
+	private String nome;
+	
+	@Column(name = "destinario_logradouro")
+	private String logradouro;
+	
+	@Column(name = "destinario_numero")
+	private String numero;
+	
+	@Column(name = "destinario_complemento")
+	private String complemento;
+	
+	@Column(name = "destinario_bairro")
+	private String bairro;
+}
