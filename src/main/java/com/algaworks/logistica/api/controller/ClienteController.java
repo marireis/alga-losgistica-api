f064ceed9d9c4f161d541aecc3965b966607f95e
Marina Reis <marinareis.mr@gmail.com>
2023-05-22 09:16:53 -0300

    endpoint put

--- src/main/java/com/algaworks/logistica/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/logistica/api/controller/ClienteController.java
@@ -2,30 +2,69 @@ package com.algaworks.logistica.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.logistica.domain.model.Cliente;
 import com.algaworks.logistica.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
-	@PersistenceContext //injenta um entity manager na variavel manage
-	private EntityManager manager;
-	
-	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	
+	@GetMapping
 	public List<Cliente> listar() {
 		
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+		
+		//OPÇÃO 2
+//		return clienteRepository.findById(clienteId)
+//				.map(cliente -> ResponseEntity.ok(cliente))
+//				.orElse(ResponseEntity.notFound().build());
+
+		//OPÇÃO 1 
+//		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+//		if(cliente.isPresent()) {
+//			return ResponseEntity.ok(cliente.get());
+//		}
+//		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente){
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(clienteId);
+		clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
+		
 	}
 
 }
--- src/main/java/com/algaworks/logistica/domain/repository/ClienteRepository.java
+++ src/main/java/com/algaworks/logistica/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.algaworks.logistica.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import com.algaworks.logistica.domain.model.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
-	
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
 }
